8facf3adcc003d684cd0dffb73f7a47ab785208f
Aaron Liblong <liblonga@hotmail.com>
2019-12-22 00:06:14 -0500

    wip:

--- autotracker.lua
+++ autotracker.lua
@@ -8,42 +8,100 @@
 -- end
 
 -- event.onmemorywrite(register_self_deleting_capture_fn, 0x801CA1D8)
-scene_changing = false
-SCENE_CHANGE_END_ADDR = 0x1DA298
+-- scene_changing = false
+-- SCENE_CHANGE_END_ADDR = 0x1DA298
+SCENE_ADDR = 0x1C8544
 
-function register_handler(addr, size, name)
+function execute_next_frame(fn, name)
     return function()
-        function handle_event_once()
-            -- local val
-            -- if size == 1 then val = mainmemory.read_u8(addr)
-            -- elseif size == 2 then val = mainmemory.read_u16_be(addr)
-            -- else val = mainmemory.read_u32_be(addr)
-            -- end
-            handle_event(name, SIZE_TO_READ_FN[size](addr))
+        function execute_then_unregister()
+            fn()
             event.unregisterbyname(name)
         end
-        event.onframeend(handle_event_once, name)
+        event.onframeend(execute_then_unregister, name)
     end
 end
 
-function handle_event(name, val)
+-- TODO: send entire inventory state when a discrepancy is found
+function resync()
+    for name, mem in pairs(LOCATION_TYPES) do
+        local addr = mem["addr"]
+        local size = mem["size"]
+        fixed_size_read_fn = SIZE_TO_READ_FN[size]
+        if fixed_size_read_fn ~= nil then
+            scene_and_global_flags[name] = fixed_size_read_fn(addr)
+        else
+            scene_and_global_flags[name] = read_range(size)(addr)
+        end
+    end
+end
+
+function resync_next_frame()
+    return execute_next_frame(resync, "resync")
+end
+
+function read_mem_and_handle(addr, size, name)
+    return function()
+        fixed_size_read_fn = SIZE_TO_READ_FN[size]
+        if fixed_size_read_fn ~= nil then
+            handle(name, fixed_size_read_fn(addr))
+        else
+            handle(name, read_range(size)(addr))
+        end
+    end
+end
+
+function read_mem_and_handle_next_frame(addr, size, name)
+    return execute_next_frame(read_mem_and_handle(addr, size, name), name)
+end
+
+function handle(name, val)
     if mainmemory.read_u32_be(0x11A5EC) == 0 then --or mainmemory.read_u32_be(0x11B91C) == 0 then
         return
     end
     if boot_sequence > 0 then
         return
     end
+    -- if scene_number == 0x48 then
+    --     if LOCATION_TYPES[name] ~= nil then
+    --         update_flags(name, val)
+    --         if store_vals ~= nil then
+    --             print(string.format("%s\t%s\t%x -> %x\t%s\t%x\t%x", os.date("%Y-%m-%d %H:%M:%S", os.time()), name, store_vals["prev_val"], store_vals["val"], last_ck_ty, last_ck_val, get_scene_number()))
+    --             store_vals = nil
+    --         else
+    --             store_vals = 1
+    --         end
+    --     else
+    --         if store_vals ~= nil then
+    --             prev_val = update_inventory(name, val)
+    --             if prev_val ~= val then
+    --                 print(string.format("%s\t%s\t%x -> %x\t%s\t%x\t%x", os.date("%Y-%m-%d %H:%M:%S", os.time()), name, prev_val, val, last_ck_ty, last_ck_val, get_scene_number()))
+    --             end
+
     if LOCATION_TYPES[name] ~= nil then
         update_flags(name, val)
     else
-        if not scene_changing then --and not in_cutscene
-            scene_number = get_scene_number()
+        --if not scene_changing then --and not in_cutscene
             -- 0x802c is val for title cutscene briefly
-            if scene_number ~= 0x802c and scene_number ~= 0x8017 and scene_number ~= 0x8018 then
-                prev_val = update_inventory(name, val)
+        scene_number = get_scene_number()
+        if scene_number ~= 0x802c and scene_number ~= 0x8017 and scene_number ~= 0x8018 then
+            prev_val = update_inventory(name, val)
+            if prev_val ~= val then
+                -- When learning the windmill song, the song/item get flag is written _before_
+                -- the "learned song of storms" and "song played in windmill" flags.
+                -- All other check logic works on the basis of storing the last flag
+                -- set between chest flags, freestanding item (collectible) flags,
+                -- song learned flags, and event item obtained (NPC and scrubs) flags.
+                -- This clause looks to see if the player is in the windmill and playing ocarina
+                -- when the check is obtained.
+                if scene_number == 0x48 and mainmemory.read_u32_be(0x1D9008) == 0x6FE260 then
+                    last_ck_ty = "misc_event_3"
+                    last_ck_val = 0x800
+                end
                 print(string.format("%s\t%s\t%x -> %x\t%s\t%x\t%x", os.date("%Y-%m-%d %H:%M:%S", os.time()), name, prev_val, val, last_ck_ty, last_ck_val, get_scene_number()))
             end
         end
+        --end
     end
 end
 
@@ -54,11 +112,12 @@ function update_inventory(name, val)
 end
 
 function update_flags(name, val)
-    prev_val = scene_flags[name]
-    ck_val = val - prev_val
+    local prev_val = scene_and_global_flags[name]
+    local ck_val = val - prev_val
     last_ck_ty = name
     last_ck_val = ck_val
-    scene_flags[name] = val
+    scene_and_global_flags[name] = val
+    print(string.format("Scene: %.02x\tCk type: %s\tCk val: %x", get_scene_number(), name, ck_val))
 end
 
 function get_scene_number()
@@ -70,10 +129,29 @@ function hexify(padding)
     return function(n) return string.format("%0" .. padding .. "x", tonumber(n)) end
 end
 
+-- for fuck's sake, when specifying a base (i.e. non-10),
+-- tonumber is capped at 2^32 - 1
+-- I think this is specific to the Bizhawk lua interpreter
+-- oh, and using string.format to represent a number greater than 2^32 - 1
+-- in hex just doesn't work
 function read_range(size)
     local function read_n_range(addr)
-        local val = tonumber(table.concat(map(hexify(2), one_index(mainmemory.readbyterange(addr, size))), ""), 16)
-        return val
+        local bytes = one_index(mainmemory.readbyterange(addr, size))
+        local hex_str_bytes = map(hexify(2), bytes)
+        local hex_str_rep = table.concat(hex_str_bytes, "")
+        -- local length = string.len(hex_str_rep)
+        -- vals = {}
+        -- i = 1
+        -- while length > 8 do
+        --     vals[i] = tonumber(string.sub(hex_str_rep, -8, -1), 16)
+        --     hex_str_rep = string.sub(hex_str_rep, 1, -9)
+        --     i = i + 1
+        -- end
+        -- vals[i] = tonumber(hex_str_rep, 16)
+
+        -- local vals = tonumber()
+        -- return vals
+        return tonumber(hex_str_rep, 16)
     end
     return read_n_range
 end
@@ -99,39 +177,11 @@ end
 SIZE_TO_READ_FN = {
     [1] = mainmemory.read_u8,
     [2] = mainmemory.read_u16_be,
-    [3] = read_range(3),
     [4] = mainmemory.read_u32_be
-}
-
-INVENTORY_ADDRS = {
-    quest_items = 0x11A675,
-    boomerang = 0x11A650,
-    hammer = 0x11A653,
-    boots_tunic_shield_sword = 0x11A66C,
-    -- the first byte of this 4-byte sequence is unused; the remaining three
-    -- are updated at the same time, and twice in a row when a check is gotten
-    -- the sequence is treated as a single word
-    stick_nut_scale_wallet_bullet_quiver_bomb_str = 0x11A670
 }
 
 last_ck_ty = ""
 last_ck_val = 0
-
-scene_flags = {
-    chest = 0,
-    collec = 0,
-    songs1 = 0,
-    songs2 = 0,
-    songs3 = 0,
-}
-
-inventory = {
-    quest_items = 0,
-    boomerang = 0,
-    hammer = 0,
-    boots_tunic_shield_sword = 0,
-    stick_nut_scale_wallet_bullet_quiver_bomb_str = 0
-}
 
 
 boot_sequence = 3
@@ -168,33 +218,101 @@ handle_scene_setup_index = function()
     end
 end
 
+-- https://wiki.cloudmodding.com/oot/Save_Format#Event_Flags
+-- The trigger address has to be the actual start of the word
+-- i.e. if a 4-byte word is written to address 0x0002, triggering off
+-- writes to 0x0005 will do nothing.
+-- I think this is mostly relevant in the writes that are patched in;
+-- i.e. the writes in the base game tend to be 1 byte at a time.
 LOCATION_TYPES = {
     chest = {addr = 0x1CA1D8, size = 4},
     collec = {addr = 0x1CA1E4, size = 4},
-    songs1 = {addr = 0x11B4AE, size = 1},
-    songs2 = {addr = 0x11B4AF, size = 1},
+    misc_event_1 = {addr = 0x11B4A4, size = 4},
+    misc_event_2 = {addr = 0x11B4A8, size = 4},
+    misc_event_3 = {addr = 0x11B4AC, size = 4},
+    -- songs1 = {addr = 0x11B4AE, size = 1},
+    -- songs2 = {addr = 0x11B4AF, size = 1},
+    misc_event_4 = {addr = 0x11B4B0, size = 4},
+    misc_event_5 = {addr = 0x11B4B4, size = 4},
     songs3 = {addr = 0x11B4B8, size = 1},
+    misc_event_6 = {addr = 0x11B4B9, size = 1},
+    saria_bridge = {addr = 0x11B4BA, size = 4},
+    skulltula_token_turnin_checks = {addr = 0x11B4BE, size = 1},
+    frog_checks = {addr = 0x11B4BF, size = 1},
+    -- not sure if it's lua's fault or Bizhawk's, but this implementation
+    -- of lua interpreter doesn't deal well with numbers > 2^32 - 1
+    npc_scrub1 = {addr = 0x11B4C0, size = 4},
+    npc_scrub2 = {addr = 0x11B4C4, size = 4},
+    link_the_goron = {addr = 0x11B4E8, size = 4},
+    thaw_king_zora = {addr = 0x11B4EC, size = 4},
+    nut_stick_richard_horsebackarchery = {addr = 0x11B4F8, size = 4},
 }
+scene_and_global_flags = {}
 
 for name, mem in pairs(LOCATION_TYPES) do
-    event.onmemorywrite(register_handler(mem["addr"], mem["size"], name), 0x80000000 + mem["addr"])
+    scene_and_global_flags[name] = 0
+    event.onmemorywrite(read_mem_and_handle_next_frame(mem["addr"], mem["size"], name), 0x80000000 + mem["addr"])
 end
 
--- event.onmemorywrite(register_handler(0x1CA1D8, 4, "chest"), 0x801CA1D8)
--- event.onmemorywrite(register_handler(0x1CA1E4, 4, "collec"), 0x801CA1E4)
--- event.onmemorywrite(register_handler(0x11BC2E, 1, "songs1"), 0x8011BC2E)
--- event.onmemorywrite(register_handler(0x11BC2F, 1, "songs2"), 0x8011BC2F)
--- event.onmemorywrite(register_handler(0x11BC38, 1, "songs3"), 0x8011BC38)
+INVENTORY_MEM_BLOCKS = {
+    fire_arrow = {addr = 0x11A648, size = 1},
+    dins_fire = {addr = 0x11A649, size = 1},
+    ocarina = {addr = 0x11A64B, size = 1},
+    bombchu = {addr = 0x11A64C, size = 1},
+    hookshot = {addr = 0x11A64D, size = 1},
+    farores_wind = {addr = 0x11A64F, size = 1},
+    boomerang = {addr = 0x11A650, size = 1},
+    lens = {addr = 0x11A651, size = 1},
+    beans = {addr = 0x11A652, size = 1},
+    hammer = {addr = 0x11A653, size = 1},
+    light_arrow = {addr = 0x11A654, size = 1},
+    nayrus_love = {addr = 0x11A655, size = 1},
+    bottle1 = {addr = 0x11A656, size = 1},
+    bottle2 = {addr = 0x11A657, size = 1},
+    bottle3 = {addr = 0x11A658, size = 1},
+    bottle4 = {addr = 0x11A659, size = 1},
+    child_trade = {addr = 0x11A65A, size = 1},
+    adult_trade = {addr = 0x11A65B, size = 1},
+    boots_tunic_shield_sword = {addr = 0x11A66C, size = 2},
+    -- the first byte of this 4-byte sequence is unused; the remaining three
+    -- are updated at the same time, and twice in a row when a check is gotten
+    -- the sequence is treated as a single word
+    stick_nut_scale_wallet_bullet_quiver_bomb_str = {addr = 0x11A670, size = 4},
+    quest_items = {addr = 0x11A674, size = 4},
+}
+inventory_state = {}
+
+for name, mem in pairs(INVENTORY_MEM_BLOCKS) do
+    inventory_state[name] = 0
+    event.onmemorywrite(read_mem_and_handle_next_frame(mem["addr"], mem["size"], name), 0x80000000 + mem["addr"])
+end
+
+event.onmemorywrite(read_mem_and_handle_next_frame(INVENTORY_ADDRS["stick_nut_scale_wallet_bullet_quiver_bomb_str"], 4, "stick_nut_scale_wallet_bullet_quiver_bomb_str"), INVENTORY_ADDRS["stick_nut_scale_wallet_bullet_quiver_bomb_str"] + 0x80000000)
+event.onmemorywrite(read_mem_and_handle_next_frame(INVENTORY_ADDRS["hammer"], 1, "hammer"), INVENTORY_ADDRS["hammer"] + 0x80000000)
+event.onmemorywrite(read_mem_and_handle_next_frame(INVENTORY_ADDRS["boomerang"], 1, "boomerang"), INVENTORY_ADDRS["boomerang"] + 0x80000000)
+event.onmemorywrite(read_mem_and_handle_next_frame(INVENTORY_ADDRS["boots_tunic_shield_sword"], 2, "boots_tunic_shield_sword"), INVENTORY_ADDRS["boots_tunic_shield_sword"] + 0x80000000)
+event.onmemorywrite(read_mem_and_handle_next_frame(INVENTORY_ADDRS["quest_items"], 3, "quest_items"), INVENTORY_ADDRS["quest_items"] + 0x80000000)
+
+inventory = {
+    quest_items = 0,
+    boomerang = 0,
+    hammer = 0,
+    boots_tunic_shield_sword = 0,
+    stick_nut_scale_wallet_bullet_quiver_bomb_str = 0
+}
+
+-- event.onmemorywrite(read_mem_and_handle_next_frame(0x1CA1D8, 4, "chest"), 0x801CA1D8)
+-- event.onmemorywrite(read_mem_and_handle_next_frame(0x1CA1E4, 4, "collec"), 0x801CA1E4)
+-- event.onmemorywrite(read_mem_and_handle_next_frame(0x11BC2E, 1, "songs1"), 0x8011BC2E)
+-- event.onmemorywrite(read_mem_and_handle_next_frame(0x11BC2F, 1, "songs2"), 0x8011BC2F)
+-- event.onmemorywrite(read_mem_and_handle_next_frame(0x11BC38, 1, "songs3"), 0x8011BC38)
 -- the +1 here means the trigger doesn't depend on the first byte, which holds no useful information
-event.onmemorywrite(register_handler(INVENTORY_ADDRS["stick_nut_scale_wallet_bullet_quiver_bomb_str"], 4, "stick_nut_scale_wallet_bullet_quiver_bomb_str"), INVENTORY_ADDRS["stick_nut_scale_wallet_bullet_quiver_bomb_str"] + 0x80000000)
-event.onmemorywrite(register_handler(INVENTORY_ADDRS["hammer"], 1, "hammer"), INVENTORY_ADDRS["hammer"] + 0x80000000)
-event.onmemorywrite(register_handler(INVENTORY_ADDRS["boomerang"], 1, "boomerang"), INVENTORY_ADDRS["boomerang"] + 0x80000000)
-event.onmemorywrite(register_handler(INVENTORY_ADDRS["boots_tunic_shield_sword"], 2, "boots_tunic_shield_sword"), INVENTORY_ADDRS["boots_tunic_shield_sword"] + 0x80000000)
-event.onmemorywrite(register_handler(INVENTORY_ADDRS["quest_items"], 3, "quest_items"), INVENTORY_ADDRS["quest_items"] + 0x80000000)
 
 event.onmemorywrite(reboot_detect, 0x8017CA48)
 event.onmemorywrite(file_select, 0x8011A5EC)
 event.onmemorywrite(handle_scene_setup_index, 0x8011B930)
+
+event.onmemorywrite(resync_next_frame(), SCENE_ADDR + 0x80000000)
 -- event.onmemorywrite(function() scene_changing = true end, 0x801C8544)
 -- event.onmemorywrite(function() scene_changing = false end, 0x801DA298)
 -- dive: +0x02 per upgrade
